3fd82b99ed2c1bcb97c94e76d643143590043b05
guillaumepoucet <poucet@simplon-charleville.fr>
2020-04-09 11:58:55 +0200

    09/04/20

--- admin/db-manage.php
+++ admin/db-manage.php
@@ -1,19 +1,19 @@
 <div id="admin-film">
 
-<div class="sub-menu">
-    <h5>Gérer les films</h5>
-    <ul><a href="#filmAdd">Ajouter un film</a></ul>
-    <ul><a href="#filmDelete">Supprimer un film</a></ul>
-    <ul><a href="#filmEdit">&Eacute;diter un film</a></ul>
-    <h5>Gérer les acteurs</h5>
-    <ul><a href="#actorAdd">Ajouter des acteurs</a></ul>
-    <ul><a href="#actortofilm">Ajouter des acteurs à un film</a></ul>
-    <ul><a href="#actorDelete">Supprimer des acteurs</a></ul>
-    <ul><a href="#actorEdit">&Eacute;diter un acteur</a></ul>
-    
-</div>
+    <div class="sub-menu">
 
-<?php include 'include/db-manage-film.php' ?>    
-<?php include 'include/db-manage-actor.php' ?>    
+        <h5>Gérer les films</h5>
+        <button class="subtablinks" onclick="openSubTab('filmAdd', this)" id="defaultSubOpen">Ajouter un film</button>
+        <button class="subtablinks" onclick="openSubTab('filmDelete', this)">Supprimer un film</button>
+        <button class="subtablinks" onclick="openSubTab('filmEdit', this)">&Eacute;diter un film</button>
+        <h5>Gérer les acteurs</h5>
+        <button class="subtablinks" onclick="openSubTab('actorAdd', this)">Ajouter des acteurs</button>
+        <button class="subtablinks" onclick="openSubTab('actorDelete', this)">Supprimer des acteurs</button>
+        <button class="subtablinks" onclick="openSubTab('actorEdit', this)">&Eacute;diter un acteur</button>
+
+    </div>
+
+    <?php include 'include/db-manage-film.php' ?>
+    <?php include 'include/db-manage-actor.php' ?>
 
 </div>
---
+++ include/db-actor-edit.php
@@ -0,0 +1,100 @@
+<?php
+require_once '../functions/auth.php';
+user_online();
+header('Content-type: text/html; charset=utf-8');
+require_once '../styleswitcher.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue, <?=ucwords($_SESSION['prenom'])?></title>
+
+    <link rel="stylesheet" href="../css/reset.css">
+
+    <link rel="stylesheet" media="screen, projection" type="text/css" id="css" href="../index.css" />
+
+    <!--GOOGLE FONTS-->
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Baloo+Tammudu+2:400,500,600,700,800|Ubuntu:300,300i,400,400i,500,500i,700,700i&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Rubik:300,300i,400,400i,500,500i,700,700i,900,900i&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Asap:400,400i,500,500i,600,600i,700,700i|Bellota+Text:300,300i,400,400i,700,700i&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron:700,800,900|Quicksand:300,400,500,600,700&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+
+</head>
+
+<body>
+
+    <?php include 'connectBDD.php'; ?>
+
+    <?php   
+    $id = $_POST['acteurs'];                        
+    $req = $bdd->prepare('SELECT * FROM acteurs WHERE id_acteur =' .$id);
+    $req ->execute();
+    $acteur = $req->fetch()
+    ?>
+
+<div class="db-film-edit">
+    <h3>Vous modifiez <?=$acteurs['prenom']." ".$acteurs['nom']?></h3>
+
+        <form action="../traitement/actor-edit.php" method="POST" enctype="multipart/form-data">
+            
+            <?php $_SESSION['acteur'] = $acteur['id_acteur'] ?>
+        
+            <label for="nom">Titre du film</label>
+            <input type="text" id="nom" name="nom" placeholder="<?=$film['nom']?>"><br>
+
+            <label for="nom">Titre du film</label>
+            <input type="text" id="nom" name="nom" placeholder="<?=$film['nom']?>"><br>
+
+            <label for="dateSortie">Date de sortie : 
+                <?php if(isset($film['dateSortie'])):?>
+                    <?=strftime('%d/%m/%Y', strtotime($film['dateSortie']))?>
+                    <?php endif ?>
+            </label>
+            <input type="date" id="dateSortie" name="dateSortie" placeholder=""><br>
+
+            <?=$film['dateSortie']?>
+            <label for="trailer">Trailer</label>
+            <input type="text" id="trailer" name="trailer" placeholder="<?=$film['trailer']?>"><br>
+
+            <label for="duree">Durée</label>
+            <input type="number" id="duree" name="duree" placeholder="<?=$film['duree']?>"><br>
+            <p>*en minutes</p><br>
+
+            <label for="synopsis">Synopsis</label>
+            <textarea id="sujet" name="synopsis" style="height:200px" placeholder="<?=$film['synopsis']?>"></textarea><br>
+            
+            <label for="poster">Affiche</label>
+            <input type="file" id="poster" name="poster"><br>
+            
+            <br>
+            
+            <input type="submit" value="Soumettre">
+            
+        </form>
+        
+    </div>
+
+</body>
+
+</html>
